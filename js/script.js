c20b972ff90b9ed4b38666362563e80f8ea5b0d5
kaligari <github.com@soften.pl>
2014-06-06 15:51:14 +0200

    Optymalizacja, tła, gotowy szkielet.
    Signed-off-by: kaligari <github.com@soften.pl>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 jQuery(function(){
    
 $.fn.click = function(listener) {
-
     return this.each(function() {
-
        var $this = $( this );
-
        $this.on('vclick', listener);
-
     });
-
 };
 
 function initialize(){
@@ -25,51 +20,26 @@ function initialize(){
       console.log( j + ": " + $(this).attr('class') + " X: " + sWidth*j + " Y: " + sHeight*i);  
     });
   });
-     /*
-
-  $('#scr1').data('x',0).data('y',0);
-  $('#scr2').data('x',sWidth).data('y',0);
-  $('#scr3').data('x',0).data('y',sHeight);
-  $('#scr4').data('x',sWidth).data('y',sHeight);  
-       */
-  /*
-  $('#scr2,#scr4').css({left:sWidth+'px'});
-  $('#scr3,#scr4').css({top:sHeight+'px'});
-    */
-  $('#minimize').css({top:sWidth*0.02+'px',left:sWidth*0.93+'px'});
-  $('#minimize img').width(sWidth*0.05+'px');
+  if(sWidth>=sHeight)
+    $('.screen').css({'background-size':'100% auto'});
+  else  
+    $('.screen').css({'background-size':'auto 100%'});
 }
+
   $(window).resize(function() {
     initialize();
     $('#container').stop().animate({left: -$('.active').data('x')+'px',top: -$('.active').data('y')+'px'});
   });
   $(window).load(function() {
     initialize();
-    /*
-    setTimeout(function(){
-      $('body').stop().transition({ scale: 1,duration: 2000, easing:'easeOutExpo' });
-      $('#minimize').fadeIn();
-    },1000);
-    */    
-    $('.screen').click(function(){            
-      $('#container').stop().animate({left: -$(this).data('x')+'px',top: -$(this).data('y')+'px'});//,function(){
-        $('#minimize').fadeIn();
-        //$('body').stop().transition({ scale: 1 });
-                
-      //});
-    });
     $('#minimize img').click(function(){
-      //$('#container').stop().animate({left: '-512px',top: '-384px'},function(){
-        $('#container').stop().animate({left: '0px',top: '0px'});
-        $('#minimize').fadeOut();
-        //$('body').stop().transition({ scale: 0.5 });              
-      //});
+        $('#container').stop().animate({left: '0px',top: '0px'});        
     });
-    function changeTo(target){
+    function changeTo(target,current){
       $('#container').stop().animate({left: -target.data('x')+'px'},function(){
         $('#container').stop().animate({top: -target.data('y')+'px'});
       });
-      $(this).removeClass('active');
+      current.removeClass('active');
       target.addClass('active');
     }
     
@@ -79,45 +49,45 @@ function initialize(){
             case 'left':
               target = this.next();                              
               if(target.length != 0 )                 
-                changeTo(target);  
+                changeTo(target,$(this));  
               else {
                 target = this.parent().next().children().first();
                 if(target.length != 0 )                 
-                  changeTo(target); 
+                  changeTo(target,$(this)); 
                 else {
-                  changeTo($('#container .row').first().children().first());
+                  changeTo($('#container .row').first().children().first(),$(this));
                 }
               }
             break;
             case 'right':
               target = this.prev();   
               if(target.length != 0 ){
-                changeTo(target);  
+                changeTo(target,$(this));  
               } else {
                 target = this.parent().prev().children().last();
                 if(target.length != 0 )                 
-                  changeTo(target); 
+                  changeTo(target,$(this)); 
                 else {
-                  changeTo($('#container .row').last().children().last());
+                  changeTo($('#container .row').last().children().last(),$(this));
                 }
               }
             break;
             case 'up':              
               target = this.parent().next().children().first();             
               if(target.length != 0 ){                 
-                changeTo(target);
+                changeTo(target,$(this));
               } else {                
                 target = $('#container .row').first().children().first(); 
-                changeTo(target);
+                changeTo(target,$(this));
               } 
             break;
             case 'down':
               target = this.parent().prev().children().first();                              
               if(target.length != 0 ){                 
-                changeTo(target);
+                changeTo(target,$(this));
               } else {
                 target = $('#container .row').last().children().first(); 
-                changeTo(target);                                       
+                changeTo(target,$(this));                                       
               }   
             break;            
           }
