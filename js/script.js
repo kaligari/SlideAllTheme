a00e3e7df6b14317e16880cd760fea41bfa409c1
kaligari <github.com@soften.pl>
2014-06-06 12:11:34 +0200

    Płynne i responsywne przejścia
    Signed-off-by: kaligari <github.com@soften.pl>

--- js/script.js
+++ js/script.js
@@ -22,8 +22,7 @@ function initialize(){
   $('#scr1').data('x',0).data('y',0);
   $('#scr2').data('x',sWidth).data('y',0);
   $('#scr3').data('x',0).data('y',sHeight);
-  $('#scr4').data('x',sWidth).data('y',sHeight);
-  
+  $('#scr4').data('x',sWidth).data('y',sHeight);  
   
   $('#scr2,#scr4').css({left:sWidth+'px'});
   $('#scr3,#scr4').css({top:sHeight+'px'});
@@ -33,19 +32,20 @@ function initialize(){
 }
   $(window).resize(function() {
     initialize();
+    $('#container').stop().animate({left: -$('.active').data('x')+'px',top: -$('.active').data('y')+'px'});
   });
   $(window).load(function() {
     initialize();
-    
+    /*
     setTimeout(function(){
       $('body').stop().transition({ scale: 1,duration: 2000, easing:'easeOutExpo' });
       $('#minimize').fadeIn();
     },1000);
-    
-    $('.screen').click(function(){      
+    */    
+    $('.screen').click(function(){            
       $('#container').stop().animate({left: -$(this).data('x')+'px',top: -$(this).data('y')+'px'});//,function(){
         $('#minimize').fadeIn();
-        $('body').stop().transition({ scale: 1 });
+        //$('body').stop().transition({ scale: 1 });
                 
       //});
     });
@@ -53,7 +53,7 @@ function initialize(){
       //$('#container').stop().animate({left: '-512px',top: '-384px'},function(){
         $('#container').stop().animate({left: '0px',top: '0px'});
         $('#minimize').fadeOut();
-        $('body').stop().transition({ scale: 0.5 });              
+        //$('body').stop().transition({ scale: 0.5 });              
       //});
     });
     $('.screen').swipe({
@@ -61,23 +61,35 @@ function initialize(){
           switch(direction){
             case 'left':
               var next = $(this).data('right');
-              if(next)
-                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});  
+              if(next){
+                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});
+                $(this).removeClass('active');
+                $('#'+next).addClass('active');
+              }  
             break;
             case 'right':
               var next = $(this).data('left');
-              if(next)
-                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});  
+              if(next){
+                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});
+                $(this).removeClass('active');
+                $('#'+next).addClass('active');
+              }  
             break;
             case 'down':
               var next = $(this).data('up');
-              if(next)
-                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});  
+              if(next){
+                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});
+                $(this).removeClass('active');
+                $('#'+next).addClass('active');
+              }  
             break;
             case 'up':
               var next = $(this).data('down');
-              if(next)
-                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});  
+              if(next){
+                $('#container').stop().animate({left: -$('#'+next).data('x')+'px',top: -$('#'+next).data('y')+'px'});
+                $(this).removeClass('active');
+                $('#'+next).addClass('active');
+              }  
             break;
           }
         }
